95fd657ac4f3262c6ebd394df9a78bc785cc8638
root <root@ip-172-31-15-52.ap-south-1.compute.internal>
2020-05-24 04:09:07 +0000

    src/App.js changed

--- src/App.js
+++ src/App.js
@@ -14,6 +14,12 @@ class App extends Component {
 	    Welcome to Nodejs react page.
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+	<div className="App-detail">
+		<B> GIT HUB and TRAVIS and AWS ElasticBeanStalk Information </B>
+	       Numerical Computing with Python
+		Python Programming Blueprints
+		Raspberry Pi Image Processing Programming 
+	</div>
       </div>
     );
   }
